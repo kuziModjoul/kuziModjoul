2a17beb2d27b8bf55921cf2c13e7c8fc0f159b38
kuziModjoul <116364888+kuziModjoul@users.noreply.github.com>
2022-10-21 10:57:43 -0400

    Update streamlit_qrcode.py

--- streamlit_qrcode.py
+++ streamlit_qrcode.py
@@ -15,6 +15,16 @@ with col2:
     st.markdown("<center><img src='https://github.com/kuziModjoul/streamlit-webpage-example/blob/main/modjoul_logo.jpg?raw=1' width=100/></center>", unsafe_allow_html=True)
     st.markdown(" <h1 style='text-align: center;'>Congratulations you Won!!!!!!!!!</h1>", unsafe_allow_html=True)
     st.balloons()
+    
+    with st.form("my_form"):
+       st.write("Inside the form")
+       slider_val = st.slider("Form slider")
+       checkbox_val = st.checkbox("Form checkbox")
+
+       # Every form must have a submit button.
+       submitted = st.form_submit_button("Submit")
+       if submitted:
+           st.write("slider", slider_val, "checkbox", checkbox_val)
 
 with col3:
     st.write("")
