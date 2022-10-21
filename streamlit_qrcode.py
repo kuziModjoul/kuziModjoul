ae2a8c4ff90dcb5e18e71a04f3b71b0b02183c3f
kuziModjoul <116364888+kuziModjoul@users.noreply.github.com>
2022-10-21 10:19:41 -0400

    Update streamlit_qrcode.py

--- streamlit_qrcode.py
+++ streamlit_qrcode.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config( page_title="congratulations",
-                    page_icon= "random",
+#                     page_icon= "random",
                     layout="wide"
  )
 
