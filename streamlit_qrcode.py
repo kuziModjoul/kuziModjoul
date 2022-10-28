e7f6d788873cee0be17c80eb5d2c7b5ba36d1ba8
kuziModjoul <116364888+kuziModjoul@users.noreply.github.com>
2022-10-28 15:22:01 -0400

    Update streamlit_qrcode.py

--- streamlit_qrcode.py
+++ streamlit_qrcode.py
@@ -12,7 +12,7 @@ with col1:
 
 with col2:
 
-    st.markdown("<center><img src='https://github.com/kuziModjoul/streamlit-webpage-example/blob/main/modjoul_logo.jpg?raw=1' width=100/></center>", unsafe_allow_html=True)
+    st.markdown("<center><img src='https://github.com/kuziModjoul/streamlit-webpage-example/blob/main/modjoul.jpg?raw=1' width=100/></center>", unsafe_allow_html=True)
     st.markdown(" <h1 style='text-align: center;'>Congratulations you Won!!!!!!!!!</h1>", unsafe_allow_html=True)
     st.balloons()
     
